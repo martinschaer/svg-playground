6f7912c449e171a41a413522c66fa15b795d6c18
Martin Corrales Schaer <m.corrales.schaer@accenture.com>
2023-01-23 14:44:42 -0600

    Install prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 // import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
-  return (
-    <div className='container mx-auto'>
-      Hola
-    </div>
-  )
+  return <div className="container mx-auto">Hola</div>;
 }
 
-export default App
+export default App;
