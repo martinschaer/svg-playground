69c3c9415efe89633afc20bed6d73d87d93dd4ab
Martin Corrales Schaer <m.corrales.schaer@accenture.com>
2023-01-23 14:56:01 -0600

    Configure prettier with eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-// import { useState } from 'react'
+import React from "react";
 import "./App.css";
 
-function App() {
+const App: React.FC = () => {
   return <div className="container mx-auto">Hola</div>;
-}
+};
 
 export default App;
